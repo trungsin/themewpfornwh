6db920590047480bca685afde5aafa439577c192
root <root@vultr.guest>
2022-02-24 01:51:53 +0000

    commit from server

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1645967411;s:7:"authKey";s:64:"ipz7v]4U~FAOEtg%W)XYbs7@}jZ29x(=v;_Woh& ^Uhd*LD?zczW`N:ij4*_YP!P";s:7:"version";s:5:"1.0.4";}
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1645967411;s:7:"authKey";s:64:"ipz7v]4U~FAOEtg%W)XYbs7@}jZ29x(=v;_Woh& ^Uhd*LD?zczW`N:ij4*_YP!P";s:7:"version";s:5:"1.0.4";s:13:"attackDataKey";i:330;}
