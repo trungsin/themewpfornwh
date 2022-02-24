2f06626eb89f8ce38007cb313f1e5cb0f0f4d470
le sin <sin.letrung@gmail.com>
2022-02-24 16:52:01 +0700

    s

--- wp-content/themes/infinity-blog/index.php
+++ wp-content/themes/infinity-blog/index.php
@@ -12,7 +12,7 @@
  * @package Infinity Blog
  */
 
-get_header(); get_sidebar();?>
+get_header(); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
