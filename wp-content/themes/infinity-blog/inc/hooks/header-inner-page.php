676bcbb8b8d538e02a73d9a9f02efac9ec848921
le sin <sin.letrung@gmail.com>
2022-02-25 17:14:51 +0700

    s

--- wp-content/themes/infinity-blog/inc/hooks/header-inner-page.php
+++ wp-content/themes/infinity-blog/inc/hooks/header-inner-page.php
@@ -39,38 +39,7 @@ if (!function_exists('infinity_blog_single_page_title')) :
             </div>
             </header>
         </div>
-  <div class="wrapper page-inner-title inner-banner primary-bgcolor data-bg"
-             data-background="<?php echo esc_url($global_banner_image); ?>">
-            <header class="entry-header">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-10">
-                            <img src="">
-                            <?php if (is_singular()) { ?>
-                                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
-                                <div class="inner-meta-info">
-                                    <?php if( is_singular('post') ){ infinity_blog_posted_details(); } ?>
-                                </div>
-                            <?php } elseif (is_404()) { ?>
-                                <h1 class="entry-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'infinity-blog'); ?></h1>
-                            <?php } elseif (is_archive()) {
-                                the_archive_title('<h1 class="entry-title">', '</h1>'); ?>
-                                <?php the_archive_description('<div class="taxonomy-description">', '</div>');
-                            } elseif (is_search()) { ?>
-                                <h1 class="entry-title"><?php printf(esc_html__('Search Results for: %s', 'infinity-blog'), '<span>' . get_search_query() . '</span>'); ?></h1>
-                            <?php } else { } ?>
-                            <?php
-                            /**
-                             * Hook - infinity_blog_add_breadcrumb.
-                             */
-                            do_action('infinity_blog_action_breadcrumb');
-                            ?>
-                        </div>
-                    </div>
-                </div>
-            </header><!-- .entry-header -->
-            <div class="inner-header-overlay"></div>
-        </div>
+  
 
         <?php
     }
