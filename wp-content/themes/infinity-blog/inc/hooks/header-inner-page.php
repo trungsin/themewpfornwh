9f71ccf6ac063b70167d25b574aa0718b7e42a01
le sin <sin.letrung@gmail.com>
2022-02-25 17:13:26 +0700

    s

--- wp-content/themes/infinity-blog/inc/hooks/header-inner-page.php
+++ wp-content/themes/infinity-blog/inc/hooks/header-inner-page.php
@@ -20,7 +20,19 @@ if (!function_exists('infinity_blog_single_page_title')) :
              <?php  the_post_thumbnail('full',['class' => 'hero-img', 'title' => 'Feature image']); ?>
                 <header class="header-single container has-img">
                         <div class="container">
-                <h1 class="entry-title">Bloody Mary Bar Tips and Recipes</h1>
+                 <?php if (is_singular()) { ?>
+                                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                                <div class="inner-meta-info">
+                                    <?php if( is_singular('post') ){ infinity_blog_posted_details(); } ?>
+                                </div>
+                            <?php } elseif (is_404()) { ?>
+                                <h1 class="entry-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'infinity-blog'); ?></h1>
+                            <?php } elseif (is_archive()) {
+                                the_archive_title('<h1 class="entry-title">', '</h1>'); ?>
+                                <?php the_archive_description('<div class="taxonomy-description">', '</div>');
+                            } elseif (is_search()) { ?>
+                                <h1 class="entry-title"><?php printf(esc_html__('Search Results for: %s', 'infinity-blog'), '<span>' . get_search_query() . '</span>'); ?></h1>
+                            <?php } else { } ?>
                 <div class="meta right">
     <p class="last-updated">Last Updated: <time class="published" datetime="2019-08-09T12:35:05-07:00">August 9, 2019</time></p>
     </div>
