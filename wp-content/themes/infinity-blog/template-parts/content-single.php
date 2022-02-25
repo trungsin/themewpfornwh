acd3fd18b8112ec231a69e5adc630b5807fcf09c
le sin <sin.letrung@gmail.com>
2022-02-25 17:23:12 +0700

    s

--- wp-content/themes/infinity-blog/template-parts/content-single.php
+++ wp-content/themes/infinity-blog/template-parts/content-single.php
@@ -25,19 +25,19 @@ if (empty($image_values)) {
 } else {
 	$values = esc_attr($image_values);
 }
-if ('no-image' != $values) {
-	if ('left' == $values) {
-		echo "<div class='image-left'>";
-		the_post_thumbnail('medium');
-	} elseif ('right' == $values) {
-		echo "<div class='image-right'>";
-		the_post_thumbnail('medium');
-	} else {
-		echo "<div class='image-full'>";
-		the_post_thumbnail('full');
-	}
-	echo "</div>";/*div end */
-}
+// if ('no-image' != $values) {
+// 	if ('left' == $values) {
+// 		echo "<div class='image-left'>";
+// 		the_post_thumbnail('medium');
+// 	} elseif ('right' == $values) {
+// 		echo "<div class='image-right'>";
+// 		the_post_thumbnail('medium');
+// 	} else {
+// 		echo "<div class='image-full'>";
+// 		the_post_thumbnail('full');
+// 	}
+// 	echo "</div>";/*div end */
+// }
 the_content();
 if( !class_exists( 'Booster_Extension_Class' ) ){
 
